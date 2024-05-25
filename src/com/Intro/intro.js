1ad916578e1863bd54b8a4b107b0c755ea7c315b
nayebtowki <nybtwki98@gmail.com>
2024-05-25 22:32:18 +0100

    text align fix

--- src/com/Intro/intro.js
+++ src/com/Intro/intro.js
@@ -8,7 +8,7 @@ const Intro = () => {
             <div className="introContent">
                 <span className="hi">Hi,</span>
                 <span className="introText">I'm <span className="introName">Nayeb Towki,</span> <br />a Cybersecurity Enthusiast <br />& Full-Stack Web Developer</span>
-                <p className="introPara">As a passionate technologist, I am dedicated to merging the analytical rigor of cybersecurity and the creative artistry of web development. My journey is a blend of  securing digital landscapes and crafting engaging web experiences, showcasing a unique synergy between diverse technological domains.</p>
+                <p className="introPara">As a passionate technologist, I am dedicated to merging the analytical rigor of cybersecurity and <br/> the creative artistry of web development. My journey is a blend of  securing digital landscapes <br/>and crafting engaging web experiences, showcasing a unique synergy between diverse technological domains.</p>
             <div>
                 <a href={CV} target="_blank" rel="noopener noreferrer" download>
                     <button className="btn">Download My CV</button>
